97abe3a0b10a3e62f089fd05782220e796616cd2
Djalil KHELLADI <djalil.khelladi@naldeo.com>
2024-09-06 12:51:44 +0200

    GPU use

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ from stable_baselines3.common.callbacks import CheckpointCallback
 from agar_gym_env import AgarEnv
 import argparse
 import os
+import torch
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--resume", action="store_true", help="Resume training from a checkpoint")
@@ -18,19 +19,22 @@ def create_env(render_mode=None):
 # Utiliser render_mode=None pendant l'entraînement pour désactiver le rendu
 vec_env = DummyVecEnv([lambda: create_env(render_mode=None)]) 
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 # Étape 3: Charger le modèle si on reprend l'entraînement
 if args.resume and os.path.exists(args.checkpoint_path):
     print(f"Reprise de l'entraînement à partir du checkpoint : {args.checkpoint_path}")
     model = PPO.load(args.checkpoint_path, env=vec_env, verbose=1, tensorboard_log="./ppo_agar_tensorboard/")
+    model.set_device(device)
 else:
     print("Nouvel entraînement démarré.")
-    model = PPO("CnnPolicy", vec_env, verbose=1, tensorboard_log="./ppo_agar_tensorboard/")
+    model = PPO("CnnPolicy", vec_env, verbose=1, tensorboard_log="./ppo_agar_tensorboard/", device=device)
 
 # Étape 4: Configurer un callback pour sauvegarder le modèle périodiquement
 checkpoint_callback = CheckpointCallback(save_freq=10000, save_path='./logs/', name_prefix='ppo_agar')
 
 # Étape 5: Lancer l'entraînement
-model.learn(total_timesteps=int(1e6), callback=[checkpoint_callback])
+model.learn(total_timesteps=int(1e6), callback=[checkpoint_callback], progress_bar=True)
 
 # Sauvegarder le modèle entraîné
 model.save("ppo_agar_model")
